2a37033813b2198d5729c631c65b563abb727f6c
lanuioe <lanuioe@gmail.com>
2023-12-21 14:30:37 +0900

    [learn] ajax

--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -1,7 +1,6 @@
-
-
-
-
-export * from './copy.js'
-export * from './memo.js'
-export * from './typeOf.js'
+export * from "./copy.js";
+export * from "./memo.js";
+export * from "./typeOf.js";
+export * from "./delay.js";
+export * from "./tiger.js";
+export * from "./color.js";
